587c55314688cd4453dd37c3739b5b98832244e9
narindertheblockcoders <narindertheblockcoders@gmail.com>
2022-08-08 13:16:01 +0530

    third

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import '../styles/globals.css'
 import Head from 'next/head';
  import Script from 'next/script'
 import BussinessModal from '../Component/ui/Modals/BussinessModal'
+import Link from 'next/link'
 
 
 function MyApp({ Component, pageProps }) {
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }) {
   return<>
   <Head>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
   <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
           rel="stylesheet"
