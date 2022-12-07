e2954dbfff8b40cc2cd684aa7237f0af4b867274
narindertheblockcoders <narindertheblockcoders@gmail.com>
2022-12-07 12:31:02 +0530

    mupdate

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3aeb992e0db23e3"; }
+/******/ 		__webpack_require__.h = function() { return "18ba7d66c1f477ae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
